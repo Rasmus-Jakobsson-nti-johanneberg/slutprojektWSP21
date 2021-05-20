de4c1c7f6b01ca837f94ca5b6697cba2900ca289
Rasmus-Jakobsson-nti-johanneberg <60431114+Rasmus-Jakobsson-nti-johanneberg@users.noreply.github.com>
2021-05-20 10:16:34 +0200

    Yardoc för app.rb
    Missade det igårkväll

--- app.rb
+++ app.rb
@@ -10,14 +10,24 @@ include Model
 t = Time.now
 i = 0
 
+#Gets the session of the user_id
 def get_user_id()
     return session[:id]
 end
 
+# Displays Register Page
 get('/') do
     slim(:register)
 end
 
+#Creates a new user if the length of the username and password is larger than 3 while password_confirm is equal to password.
+#Redirects to '/error' if not met with the conditions. Otherwise redirects to '/showlogin'
+#
+# @param [String] username, The username that the user input
+# @param [String] password, The password that the user input
+# @param [String] password_confirm, The confirmation that the password the user input before is correct
+#
+# @see Model#create_user
 post('/users/new') do
 
     if validate_username_length(params) == false
@@ -40,6 +50,12 @@ get('/showlogin') do
     slim(:login)
 end
 
+#Attempts to login and updates the session. Stops user from logging in with a cooldown-timer if wrong password is written more than 5 times. 
+#
+# @param [String] username, The username
+# @param [String] password, The password
+# @see Model#get_user
+# @see Model#login
 post('/login') do
     password = params[:password]
     db = get_database(params)
@@ -69,10 +85,16 @@ post('/login') do
     end
 end
 
+#Displays the error page
 get('/error') do
     slim(:error)
 end
 
+#Displays created blogs and options to update and delete created blogs. 
+#The page can only be accessed if logged in.
+#
+# @param [Integer] :id, The users ID that has been saved in a session when logged in
+# @see Model#get_user_blogs
 get("/my_blogs") do
     user_id = get_user_id()
         if user_id == 0
@@ -85,27 +107,50 @@ get("/my_blogs") do
     slim(:"my_blogs/index", locals:{my_blogs:result})
 end
 
+#Displays all blogs that are in the database
+#
+# see Model#get_blogs
+# see Model#get_genres
 get("/all_blogs") do
     blogs = get_blogs(params)
     genres = get_genres(params)
     slim(:"my_blogs/all_blogs", locals:{blogs:blogs, genres:genres})
 end
 
+#Logs out the user and redirects to '/'
 get("/logout") do
     session.destroy()
     redirect("/")
 end
 
+#Displays a create form for blogs
+#
+# @see Model#get_genres
 get("/my_blogs/create") do
     result = get_genres(params)
     slim(:"my_blogs/create", locals:{genres:result})
 end
 
+#Creates a new blog and redirects to '/my_blogs'
+#
+# @param [Integer] genre_id, The ID of the genre
+# @param [String] title, The title of the new blog
+# @param [String] recipe_id, The ID of the new blog
+# @param [Integer] :id, The users ID that has been saved in a session when logged in
+# @param [String] content, The text in the blog 
+# @see Model#create_blog
 post("/my_blogs/create") do
     create_blog = create_blog(params)
     redirect("/my_blogs")
 end
 
+#Displays an edit form for the recipe, validates the blog the user is editing is owned by the user
+#
+# @param [Integer] :id, The ID of the blog
+# @param [String] username The username
+# @see Model#validate_blogs_user
+# @see Model#get_blog
+# @see Model#get_genres
 get("/my_blogs/:id/edit") do
     if validate_blogs_user(params) == true
         result = get_blog(params)
@@ -118,6 +163,15 @@ get("/my_blogs/:id/edit") do
     end
 end
 
+#Updates an existing blog and redirects to '/my_blogs', validates the blog the user is editing is owned by the user
+#
+# @param [Integer] :id, The ID of the blog
+# @param [Integer] genre_id, The ID of the genre
+# @param [String] username The username
+# @param [String] title, The new title of the blog
+# @param [String] content, The new text in the blog
+# @see Model#validate_blogs_user
+# @see Model#update_recipe
 post("/my_blogs/:id/update") do
     if validate_blogs_user(params) == true
         updated_blog = update_blog(params)
@@ -129,6 +183,11 @@ post("/my_blogs/:id/update") do
     end
 end
 
+#Deletes an existing blog and redirects to '/my_blogs', validates the blog the user is editing is owned by the user
+#
+# @param [Integer] :id, The ID of the blog
+# @param [String] username The username
+# @see Model#blog
 post("/my_blogs/:id/delete") do
     if validate_blogs_user(params) == true
         delete = delete_blog(params)
@@ -140,12 +199,23 @@ post("/my_blogs/:id/delete") do
     end
 end
 
+#Displays all blogs that belong to the genre ID
+#
+# @param [Integer] :id, The ID of the genre
+# see Model#get_blogs_with_genre
+# see Model#get_genre
 get("/genre/:id") do
     blogs = get_blogs_with_genre(params)
     genre = get_genre(params)
     slim(:"my_blogs/all_blogs_genre", locals:{blogs:blogs, genre:genre})
 end
 
+#Displays a blog in more detail
+#
+# @param [Integer] :id, The ID of the blog
+# see Model#get_blog
+# see Model#get_blog_author
+# see Model#get_genre
 get("/blog/:id") do
     result = get_blog(params)
     author = get_blog_author(params)
